e2d868b6ed53283a70274e94eb14a8862021c851
Ruthvik <Ruthvik.Thiriveedhi@tesco.com>
2021-05-16 16:35:11 +0530

    added a thread to call draw on the surface and created a grid required for the game.

---
+++ app/src/main/java/com/ruthvik/apps/a2048/GameThread.kt
@@ -0,0 +1,64 @@
+package com.ruthvik.apps.a2048
+
+import android.graphics.Canvas
+import android.view.SurfaceHolder
+
+class GameThread (
+    var surfaceHolder: SurfaceHolder,
+    private val gameView: GameView
+) : Thread() {
+
+    private val targetFps = 60
+
+    private var canvas: Canvas? = null
+
+    private var running: Boolean = false
+
+    fun setRunning(isRunning: Boolean) {
+        this.running = isRunning
+    }
+
+    override fun run() {
+        super.run()
+        var startTime: Long = 0
+        var timeInMillis: Long = 0
+        var waitTime: Long = 0
+        val targetTime = 1000/targetFps
+
+        while(running) {
+            startTime = System.nanoTime()
+            canvas = null
+
+            try {
+                canvas = surfaceHolder.lockCanvas()
+                synchronized(surfaceHolder) {
+                    gameView.apply {
+                        draw(canvas)
+                    }
+                }
+            }catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                canvas?.let {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas)
+                    }catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+
+            timeInMillis = (System.nanoTime() - startTime)/1000000
+            waitTime = targetTime - timeInMillis
+
+            try {
+                if(waitTime > 0) {
+                    sleep(waitTime)
+                }
+            }catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/ruthvik/apps/a2048/GameView.kt
+++ app/src/main/java/com/ruthvik/apps/a2048/GameView.kt
@@ -1,6 +1,7 @@
 package com.ruthvik.apps.a2048
 
 import android.content.Context
+import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
@@ -9,6 +10,8 @@ import com.ruthvik.apps.a2048.sprites.Grid
 class GameView : SurfaceView, SurfaceHolder.Callback {
 
     private lateinit var grid: Grid
+
+    private var gameThread: GameThread? = null
 
     constructor(context: Context) : super(context) {
         initGameAndViews()
@@ -29,7 +32,10 @@ class GameView : SurfaceView, SurfaceHolder.Callback {
     }
 
     override fun surfaceCreated(holder: SurfaceHolder) {
-
+        gameThread = GameThread(holder, this).also {
+            it.setRunning(true)
+            it.start()
+        }
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
@@ -38,5 +44,14 @@ class GameView : SurfaceView, SurfaceHolder.Callback {
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
 
+    }
+
+    override fun draw(canvas: Canvas?) {
+        super.draw(canvas)
+
+        canvas?.let {
+            it.drawRGB(255,255,255) // white background for the screen
+            grid.draw(canvas)
+        }
     }
 }
