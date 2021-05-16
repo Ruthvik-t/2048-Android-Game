80b8055ad15f7d7732d6b20c5478f5a0f420ec80
Ruthvik <Ruthvik.Thiriveedhi@tesco.com>
2021-05-16 16:17:30 +0530

    added sprite interface and grid class to show in surface.

--- app/src/main/java/com/ruthvik/apps/a2048/GameView.kt
+++ app/src/main/java/com/ruthvik/apps/a2048/GameView.kt
@@ -4,12 +4,29 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import com.ruthvik.apps.a2048.sprites.Grid
 
 class GameView : SurfaceView, SurfaceHolder.Callback {
 
-    constructor(context: Context) : super(context)
+    private lateinit var grid: Grid
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context) : super(context) {
+        initGameAndViews()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initGameAndViews()
+    }
+
+    private fun initGameAndViews() {
+        holder.addCallback(this)
+
+        val screenWidth = context.resources.displayMetrics.widthPixels
+        val screenHeight = context.resources.displayMetrics.heightPixels
+        val standardSize: Int = ((screenWidth * .88)/4).toInt() // standard size of tile
+
+        grid = Grid(context.resources, screenWidth, screenHeight, standardSize)
+    }
 
     override fun surfaceCreated(holder: SurfaceHolder) {
 
---
+++ app/src/main/java/com/ruthvik/apps/a2048/sprites/Grid.kt
@@ -0,0 +1,28 @@
+package com.ruthvik.apps.a2048.sprites
+
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import com.ruthvik.apps.a2048.R
+
+class Grid constructor(
+    private val resources: Resources,
+    private val screenWidth: Int,
+    private val screenHeight: Int,
+    private val standardSize: Int,
+): Sprite {
+
+    private var grid: Bitmap? = null
+
+    init {
+        val bitmap: Bitmap = BitmapFactory.decodeResource(this.resources, R.drawable.grid)
+        grid = Bitmap.createScaledBitmap(bitmap, standardSize*4, standardSize * 4, false)
+    }
+
+    override fun draw(canvas: Canvas) {
+        grid?.let {
+            canvas.drawBitmap(it, screenWidth/2 - it.width/2f, screenHeight/2 - it.height/2f,  null )
+        }
+    }
+}
