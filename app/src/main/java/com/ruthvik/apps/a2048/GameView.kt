64ff91c313587df5fc6a91cc81b5701650a20bfa
Ruthvik <Ruthvik.Thiriveedhi@tesco.com>
2021-05-16 16:03:07 +0530

    added drawables for game and created initial custom game surface view

---
+++ app/src/main/java/com/ruthvik/apps/a2048/GameView.kt
@@ -0,0 +1,25 @@
+package com.ruthvik.apps.a2048
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class GameView : SurfaceView, SurfaceHolder.Callback {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+
+    }
+}
